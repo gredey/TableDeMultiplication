742862496b695ab469a5c6eda5bfd218dc2a53ff
gredey <osaidoupro@gmail.com>
2019-03-18 22:28:53 +0000

    Ajout du fichier table.php

---
+++ table.php
@@ -0,0 +1,21 @@
+!DOCTYPE html>
+	<html>
+		<head>
+			<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    		<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    		<title>Table de multipilication</title>
+			<link type="css" href="bootstrap.css"/>
+   			<script src="jquery.js"> </script>
+		</head>
+		<body>
+			<h1>TABLE DE MULTIPLICATION</h1>
+			<div class="row" style="border: 2px">
+				<div class="col-xs-12" style="border: 2px">
+					<label>Entrer la table de : </label> 
+					<input type="text" class="form-control" id="chiffre" />
+					<input type="button" class="" id="but" value="Afficher" />
+				</div>
+			</div>
+		</body>
+		
+	</html>
